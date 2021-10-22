c4520a4b5e5df83c4124b7b66aeb4e66566408c2
pdlmn <djeinwings@gmail.com>
2021-10-23 02:55:25 +0300

    Made calculator methods and refactored code

--- calculator.js
+++ calculator.js
@@ -1,46 +1,62 @@
 const digitButtons = document.querySelectorAll('[data-digit]');
 const operatorButtons = document.querySelectorAll('[data-operator]');
-const initialNumDiv = document.querySelector('#initial-num-row');
-const currentNumDiv = document.querySelector('#current-num-row');
+const operationDiv = document.querySelector('#operation-row');
+const inputDiv = document.querySelector('#input-row');
+const equalsButton = document.querySelector('#equals');
 
-let x = 0
+let operator = '';
+
 const calculator = {
   'add': function() {
-    return this.initialNum + this.currentNum;
+    return this.resultNum + this.currentNum;
   },
-  'subtract': function() {},
-  'clear': function() {
-    currentNumDiv.textContent = '';
+  'subtract': function() {
+    return this.resultNum - this.currentNum;
   },
+  'multiply': function() {
+    return this.resultNum * this.currentNum;
+  },
+  'divide': function() {
+    return this.resultNum / this.currentNum;
+  }
 }
 
 function addDigitToDisplay(e) {
   digit = e.target.dataset.digit;
-  let dotInDisplay = currentNumDiv.textContent.includes('.');
+  let dotInDisplay = inputDiv.textContent.includes('.');
   if (digit === '.' && dotInDisplay) {
     return
   }
-  currentNumDiv.textContent += digit;
+  inputDiv.textContent += digit;
 }
 
 function calculate(e) {
-  if (!calculator.initialNum) {
-    calculator.initialNum = Number(currentNumDiv.textContent);
-    console.log('Firstnum is: ' + calculator.initialNum);
-    return
-  }
-  calculator.currentNum = Number(currentNumDiv.textContent);
-  console.log('secondnum is: ' + calculator.currentNum);
+  if (!inputDiv.textContent) return
   operator = e.target.dataset.operator;
-  calculator.initialNum = calculator[operator]();
-  console.log('result is: ' + calculator.initialNum);
+  if (!calculator.resultNum) {
+    calculator.resultNum = Number(inputDiv.textContent);
+    operationDiv.textContent = `${calculator.resultNum} ${e.target.textContent} `;
+    inputDiv.innerHTML = '<br>';
+    console.log(operator);
+    return
+  } 
+  calculator.currentNum = Number(inputDiv.textContent);
+  calculator.resultNum = calculator[operator]();
+
+  operationDiv.textContent = `${calculator.resultNum} ${e.target.textContent} `;
+  inputDiv.innerHTML = '<br>';
 }
 
-function updateDisplay() {
-  initialNumDiv.textContent = calculator.initialNum;
-  currentNumDiv.innerHTML = '<br>';
+function equals() {
+  if (!operator || !inputDiv.textContent) return
+  calculator.currentNum = Number(inputDiv.textContent);
+  calculator.resultNum = calculator[operator]();
+  operator = '';
+
+  operationDiv.textContent += `${calculator.currentNum} = ${calculator.resultNum}`;
+  inputDiv.innerHTML = '<br>';
 }
 
 digitButtons.forEach(button => button.addEventListener('click', addDigitToDisplay));
 operatorButtons.forEach(button => button.addEventListener('click', calculate));
-operatorButtons.forEach(button => button.addEventListener('click', updateDisplay));
+equalsButton.addEventListener('click', equals);
